dcec0d779e0e35136b4838ebed998e80ef7212aa
zcc <listenzcc@mail.bnu.edu.cn>
2021-03-29 10:30:00 +0800

    Add cov features to svm classification

--- README.md
+++ README.md
@@ -7,22 +7,35 @@ The data is motion data, the aim is to decode the motion state from it.
 Data is from nowhere.
 The data has been restored as .csv file.
 
--   The 1st column is label state;
--   The 2nd column is empty;
--   The remaining columns are data:
-    -   There are 120 columns in total;
-    -   Every 6 columns formate a group for 6 channels;
-    -   Every group refers the data of 10 ms;
-    -   Thus, the data refers 200ms in total.
+- The 1st column is label state;
+- The 2nd column is empty;
+- The remaining columns are data:
+  - There are 120 columns in total;
+  - Every 6 columns formate a group for 6 channels;
+  - Every group refers the data of 10 ms;
+  - Thus, the data refers 200ms in total.
 
 ## Classification
 
 ### SVC Raw Classification
 
--   File: [raw_classification.py](./raw_classification.py);
--   Method: Classification using SVC using all 120 channels as features.
+- File: [raw_classification.py](./raw_classification.py);
+- Method: Classification using SVC using all 120 channels as features.
 
 ### Visualization
 
--   File: [visualization.py](./visualization.py)
--   Method: Generate data visualization for the events and channels.
+- File: [visualization.py](./visualization.py)
+- Method: Generate data visualization for the events and channels.
+
+### CNN Classification
+
+- File: [cnn.py](./cnn.py)
+- Method: Classification using CNN.
+
+### SVM Raw Cov Classification
+
+- File: [svm_raw_cov.py](./svm_raw_cov.py)
+- Method: Classification using SVC using all 120 channels and cov features across channels.
+- Detail: The 120 channels are of 20 times x 6 channels.
+  The method is computing the covariance across channels.
+  The results turn no improve.
---
+++ svm_raw_cov.py
@@ -0,0 +1,117 @@
+# %%
+import os
+import time
+import numpy as np
+import pandas as pd
+
+from sklearn import metrics
+from sklearn.model_selection import StratifiedKFold
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+
+import threading
+
+import plotly.express as px
+
+# %%
+
+
+def ravel(x):
+    return np.array([e.ravel() for e in x])
+
+
+def unravel(x):
+    return np.array([e.reshape((20, 6)) for e in x])
+
+
+scaler = StandardScaler()
+
+# %%
+data_folder = os.path.join(os.path.dirname(__file__), '..', 'data')
+
+# %%
+# Data_3d shape is (31625, 20, 6),
+#                   samples x times x channels
+# event shape is (31625,)
+data_3d = np.load(os.path.join(data_folder, 'data3.npy'))
+event = np.load(os.path.join(data_folder, 'event.npy'))
+data_3d.shape, event.shape
+
+# %%
+# data_3d = unravel(scaler.fit_transform(ravel(data_3d)))
+# data_3d.shape
+
+# %%
+d = data_3d.copy()
+d = d.transpose((0, 2, 1))
+data_cov_3d = np.array([np.cov(e) for e in d])
+data_cov_3d.shape
+
+# %%
+data = np.concatenate([ravel(e) for e in [
+    data_3d,
+    data_cov_3d]], axis=1)
+data.shape
+
+# %%
+label = event
+
+clf = make_pipeline(StandardScaler(), SVC(gamma='scale'))
+skf = StratifiedKFold(n_splits=10, shuffle=True)
+
+predict = label * 0
+
+
+class myThread(threading.Thread):
+    def __init__(self, train_idx, test_idx):
+        threading.Thread.__init__(self)
+        self.X = data[train_idx]
+        self.y = label[train_idx]
+        self.X1 = data[test_idx]
+        self.test_idx = test_idx
+
+    def run(self):
+        print('Thread starts')
+        t = time.time()
+        train_pred(self.X, self.y, self.X1, self.test_idx)
+        print(f'Thread finished, costs {time.time() -t} seconds')
+
+
+def train_pred(X, y, X1, test_idx):
+    print(f'Fitting')
+    clf.fit(X, y)
+
+    print(f'Predicting')
+    y2 = clf.predict(X1)
+
+    predict[test_idx] = y2
+    print(f'Done')
+
+
+pool = []
+for train_idx, test_idx in skf.split(data, label):
+    thd = myThread(train_idx, test_idx)
+    pool.append(thd)
+
+for thd in pool:
+    thd.start()
+
+for thd in pool:
+    thd.join()
+
+print(metrics.classification_report(label, predict))
+
+# %%
+rdf = pd.DataFrame(columns=['label', 'predict'])
+rdf['label'] = label
+rdf['predict'] = predict
+rdf.to_csv(os.path.join(
+    data_folder, f'raw_cov_classification_results-{time.time()}.csv'))
+
+# %%
+print(metrics.classification_report(label, predict))
+cm = metrics.confusion_matrix(label, predict, normalize='true')
+px.imshow(cm)
+
+# %%
